4a71f6d8d80429d2a45623a4f1076ea08ea3a6b8
4hsinyili <4hsinyili@gmail.com>
2021-05-25 12:06:45 +0800

    Mod.: modify url paths

--- Web/Diner_app/urls.py
+++ Web/Diner_app/urls.py
@@ -3,12 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('api/v1/dinerlist', views.DinerList.as_view()),
-    # path('api/v1/dinersearch', views.DinerSearch.as_view()),
-    # path('api/v1/dinerinfo', views.DinerInfo.as_view()),
-    # path('api/v1/filters', views.Filters.as_view()),
-    # path('api/v1/dinerfilters', views.DinerFilters.as_view()),
-    # path('dinerlist', views.dinerlist, name='dinerlist'),
-    # path('diner', views.dinerinfo, name='dinerinfo'),
-    # path('test', views.test, name='test')
+    path('api/v1/dinerlist', views.DinerList.as_view()),
+    path('api/v1/dinersearch', views.DinerSearch.as_view()),
+    path('api/v1/dinerinfo', views.DinerInfo.as_view()),
+    path('api/v1/filters', views.Filters.as_view()),
+    path('dinerlist', views.dinerlist, name='dinerlist'),
+    path('dinerinfo', views.dinerinfo, name='dinerinfo'),
 ]
