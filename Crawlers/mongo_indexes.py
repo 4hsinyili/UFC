fb5a60e56cdf9dc76c368889d9d332260771fdad
4hsinyili <4hsinyili@gmail.com>
2021-05-26 10:35:05 +0800

    Mod.: add matched and gm_placed to mongo

--- Crawlers/mongo_indexes.py
+++ Crawlers/mongo_indexes.py
@@ -18,5 +18,21 @@ if __name__ == '__main__':
     db['ue_detail'].create_index([('triggered_at', ASCENDING), ('uuid', ASCENDING)])
     db['fp_detail'].create_index([('triggered_at', ASCENDING), ('uuid', ASCENDING)])
     db['gm_detail'].create_index([('triggered_at', ASCENDING), ('link', ASCENDING)])
+    db['matched'].create_index([
+        ('uuid_ue', ASCENDING),
+        ('uuid_fp', ASCENDING),
+        ('uuid_matched', ASCENDING)
+        ('triggered_at_ue', ASCENDING),
+        ('triggered_at_fp', ASCENDING)
+        ])
+    db['gm_placed'].create_index([
+        ('uuid_ue', ASCENDING),
+        ('uuid_fp', ASCENDING),
+        ('uuid_gm', ASCENDING),
+        ('uuid_matched', ASCENDING)
+        ('triggered_at_ue', ASCENDING),
+        ('triggered_at_fp', ASCENDING),
+        ('triggered_at_gm', ASCENDING)
+    ])
 
     print('success')
