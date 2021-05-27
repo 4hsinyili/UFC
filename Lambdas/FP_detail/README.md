6acdf531253db95421c273b28ca2f36de555ea49
4hsinyili <4hsinyili@gmail.com>
2021-05-27 22:30:22 +0800

    Mod.: create Lambda/FP_detail

---
+++ Lambdas/FP_detail/README.md
@@ -0,0 +1,7 @@
+# PyChromeless Docker
+
+```shell script
+docker build -t pychromeless:python-slim --target=lambda .
+docker run --rm -it pychromeless:python-slim
+```
+
---
+++ Lambdas/FP_detail/src/lambda_function.py
@@ -0,0 +1,33 @@
+#  for db control
+from pymongo import MongoClient
+
+# for file handling
+import env
+
+from FP_detail import FPDinerDetailCrawler
+
+MONGO_HOST = env.MONGO_HOST
+MONGO_PORT = env.MONGO_PORT
+MONGO_ADMIN_USERNAME = env.MONGO_ADMIN_USERNAME
+MONGO_ADMIN_PASSWORD = env.MONGO_ADMIN_PASSWORD
+
+admin_client = MongoClient(MONGO_HOST,
+                           MONGO_PORT,
+                           username=MONGO_ADMIN_USERNAME,
+                           password=MONGO_ADMIN_PASSWORD)
+db = admin_client['ufc_temp']
+
+target = {
+    'title': 'Appworks School',
+    'address': '110台北市信義區基隆路一段178號',
+    'gps': (25.0424488, 121.562731)
+}
+
+
+def lambda_handler(event, context, *args, **kwargs):
+    index = event
+    # index = {'offset': 0, 'limit': 10}
+    offset = index['offset']
+    limit = index['limit']
+    detail_crawler = FPDinerDetailCrawler(target, 'fp_list', offset, limit)
+    diners, error_logs = detail_crawler.main(db=db, collection='fp_detail')
