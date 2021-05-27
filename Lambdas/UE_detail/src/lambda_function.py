e50db35a8b0a905eed89634037a25e8e633774c9
4hsinyili <4hsinyili@gmail.com>
2021-05-27 22:28:41 +0800

    Mod.: create Lambda/UE_detail

---
+++ Lambdas/UE_detail/src/lambda_function.py
@@ -0,0 +1,27 @@
+#  for db control
+from pymongo import MongoClient
+
+# for file handling
+import env
+
+from UE_detail import UEDinerDetailCrawler
+
+MONGO_HOST = env.MONGO_HOST
+MONGO_PORT = env.MONGO_PORT
+MONGO_ADMIN_USERNAME = env.MONGO_ADMIN_USERNAME
+MONGO_ADMIN_PASSWORD = env.MONGO_ADMIN_PASSWORD
+
+admin_client = MongoClient(MONGO_HOST,
+                           MONGO_PORT,
+                           username=MONGO_ADMIN_USERNAME,
+                           password=MONGO_ADMIN_PASSWORD)
+db = admin_client['ufc_temp']
+
+
+def lambda_handler(event, context, *args, **kwargs):
+    index = event
+    # index = {'offset': 0, 'limit': 10}
+    offset = index['offset']
+    limit = index['limit']
+    detail_crawler = UEDinerDetailCrawler('ue_list', offset, limit)
+    diners, error_logs = detail_crawler.main(db=db, collection='ue_detail')
