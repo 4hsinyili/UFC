54649be6724d7bdd7430c8381840fbc1d547a7c1
4hsinyili <4hsinyili@gmail.com>
2021-05-31 22:53:30 +0800

    Clean Code: remove useless paths

--- Web/User_app/urls.py
+++ Web/User_app/urls.py
@@ -3,14 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register', views.register, name='register'),
-    # path('login', views.custom_login, name='login'),
-    # path('logout', views.custom_logout, name='logout'),
-    # path('api/v1/dinerlist', views.DinerList.as_view()),
-    # path('api/v1/dinersearch', views.DinerSearch.as_view()),
-    # path('api/v1/dinershuffle', views.DinerShuffle.as_view()),
-    # path('api/v1/dinerinfo', views.DinerInfo.as_view()),
-    # path('api/v1/filters', views.Filters.as_view()),
-    # path('dinerlist', views.dinerlist, name='dinerlist'),
-    # path('dinerinfo', views.dinerinfo, name='dinerinfo'),
+    path('register', views.register, name='register')
 ]
