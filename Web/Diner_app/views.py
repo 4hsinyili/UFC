473b7c5c2d123efe3b1ea447a6e5f2a40c41e42e
4hsinyili <4hsinyili@gmail.com>
2021-06-07 10:44:54 +0800

    Clean Code: comment useless import

--- Web/Diner_app/views.py
+++ Web/Diner_app/views.py
@@ -1,4 +1,4 @@
-from django.http.response import HttpResponse
+# from django.http.response import HttpResponse
 from django.shortcuts import render, redirect
 # from django.http import HttpResponse
 from rest_framework import views
