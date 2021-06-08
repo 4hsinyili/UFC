9e6cfe1b32f96b6c3a6da868414d2de68a5217b5
4hsinyili <4hsinyili@gmail.com>
2021-06-08 21:18:31 +0800

    Mod.: create a new mongodb on new ec2

--- Lambdas/get_fp_detail/src/env.py
+++ Lambdas/get_fp_detail/src/env.py
@@ -2,7 +2,11 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-MONGO_ATLAS_URI = os.getenv('MONGO_ATLAS_URI')
+MONGO_HOST_EC2 = os.getenv("MONGO_HOST_EC2")
+MONGO_PORT_EC2 = int(os.getenv("MONGO_PORT_EC2"))
+MONGO_ADMIN_USERNAME_EC2 = os.getenv("MONGO_ADMIN_USERNAME_EC2")
+MONGO_ADMIN_PASSWORD_EC2 = os.getenv("MONGO_ADMIN_PASSWORD_EC2")
+
+MONGO_EC2_URI = f"mongodb://{MONGO_ADMIN_USERNAME_EC2}:{MONGO_ADMIN_PASSWORD_EC2}@{MONGO_HOST_EC2 }"
 driver_path = os.getenv('DRIVER_PATH')
-SECRET_KEY = os.getenv('SECRET_KEY')
 PLACE_API_KEY = os.getenv("PLACE_API_KEY")
