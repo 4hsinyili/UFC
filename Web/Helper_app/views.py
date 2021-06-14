a4d68db8c9e1a585183cf32e39f8119242366d76
4hsinyili <4hsinyili@gmail.com>
2021-06-14 10:38:04 +0800

    Appearance: 404, 500, rate limited page update

--- Web/Helper_app/views.py
+++ Web/Helper_app/views.py
@@ -4,12 +4,12 @@ from django.shortcuts import render
 
 
 def handle_not_found(request, exception):
-    return render(request, 'Helper_app/404.html', {})
+    return render(request, 'Helper_app/404.html', {'current_page': '404'})
 
 
 def handle_server_error(request):
-    return render(request, 'Helper_app/500.html', {})
+    return render(request, 'Helper_app/500.html', {'current_page': '500'})
 
 
 def handle_rate_limited(request, exception):
-    return render(request, 'Helper_app/rate_limited.html', {})
+    return render(request, 'Helper_app/rate_limited.html', {'current_page': 'Rate Limited'})
