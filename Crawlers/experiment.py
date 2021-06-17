9044c9187fd9d524c2f5d1e511157443176e7746
4hsinyili <4hsinyili@gmail.com>
2021-06-17 23:20:10 +0800

    Mod.: new experiment

--- Crawlers/experiment.py
+++ Crawlers/experiment.py
@@ -1,15 +1,1 @@
-import configparser
-import utils
-
-
-config = configparser.ConfigParser()
-
-config.read('crawler.conf')
-print(config['Food_Panda_Api_Url']['list_url'])
-
-headers = utils.read_json('headers_ue.json')
-
-print(type(headers))
-
-
-print((0 > False))
+import conf
