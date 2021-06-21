38b7c7098e45b6d51e098358b7e2e06df45079ea
4hsinyili <4hsinyili@gmail.com>
2021-06-21 17:37:11 +0800

    Mod.: add test_ip view to check lambda's ip

--- Web/Diner_app/urls.py
+++ Web/Diner_app/urls.py
@@ -15,5 +15,6 @@ urlpatterns = [
     path('favorites', views.favorites_view, name='favorites'),
     path('dashboard', views.dashboard_view, name='dashboard'),
     path('test_500', views.test_500_view, name='test_500'),
+    path('test_ip', views.test_lambda_ip_view, name='test_ip'),
     # path('.well-known/pki-validation/F6B43E81B6BB7366178E7DE03B1BB0FC.txt', views.forSSL, name='test')
 ]
